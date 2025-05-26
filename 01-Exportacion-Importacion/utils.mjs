266544fa0db5a49d68919cd48970e4cee15607b9
FrancoSimon <francoolmedo@live.com>
2025-05-26 19:43:48 -0300

    01-Exportacion-Importacion terminado

--- 01-Exportacion-Importacion/utils.mjs
+++ 01-Exportacion-Importacion/utils.mjs
@@ -42,8 +42,43 @@ export function leerSuperheroes(ruta) {
         )
     );
 
-    // Ordenar por nombre de superheroe (A-Z)
+    // Ordenar por nombre de superheroe
     superheroes.sort((a, b) => a.nombreSuperheroe.localeCompare(b.nombreSuperheroe));
        
     return superheroes;
+}
+
+
+//Nueva funcion para agregar superheroes
+export function agregarSuperheroes(rutaOriginal, rutaNuevos) {
+    const datosOriginales = fs.readFileSync(rutaOriginal, 'utf8');
+    const datosNuevos = fs.readFileSync(rutaNuevos, 'utf8');
+
+    const superheroesOriginales = JSON.parse(datosOriginales);
+    const nuevosSuperheroes = JSON.parse(datosNuevos);
+
+
+    //Convertir los nuevos superheroes a instancias de Superheroe
+    const instanciasNuevos = nuevosSuperheroes.map(
+        hero => new Superheroe(hero.id,
+            hero.nombreSuperheroe,
+            hero.nombreReal,
+            hero.nombreSociedad,
+            hero.edad,
+            hero.planetaOrigen,
+            hero.debilidad,
+            hero.poder,
+            hero.habilidadEspecial,
+            hero.aliado,
+            hero.enemigo
+        )
+    );
+    //Combinar listas
+    const listaActualizada = [...superheroesOriginales, ...instanciasNuevos];
+
+    //Guardar la lista actualizada
+    fs.writeFileSync(rutaOriginal, JSON.stringify(listaActualizada, null, 2), 'utf8');
+    console.log('Lista de superheroes actualizada con exito.');
+    
+    
 }
