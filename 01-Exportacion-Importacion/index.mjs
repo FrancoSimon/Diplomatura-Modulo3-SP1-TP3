1f70d3b1028867f3fd44f90c75e348710cb3e9ef
FrancoSimon <francoolmedo@live.com>
2025-05-26 17:31:54 -0300

    agrego carpetas ordenadas

---
+++ 01-Exportacion-Importacion/index.mjs
@@ -0,0 +1,6 @@
+import {leerSuperheroes} from './utils.mjs'
+
+//leer y mostrar la lista de superheroes ordenada
+const superheroes=leerSuperheroes('./superheroes.txt');
+console.log('Superheroes ordenados:');
+console.log(superheroes);
